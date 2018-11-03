e02267ec501fb01f2c104c6d1e0119d5cd179a31
Abularach <Luisabularach48@gmail.com>
2018-11-03 12:17:54 -0700

    Added priority level + other stuff

--- Gaze Detection/face_forward.py
+++ Gaze Detection/face_forward.py
@@ -1,5 +1,40 @@
+from PIL import Image, ImageDraw
 import face_recognition
+import sys
+import cv2
+
+def main():
+    #Test with image
+    dir = "test_images/"
+    # Load the jpg file into a numpy array
+    image = face_recognition.load_image_file(dir + sys.argv[1] + ".png")
+    facial_features = isLookingForward(image)
+    display(facial_features)
+
 
 def isLookingForward(image):
-    #Check if person is looking straight forward
-    #Examples of mapping facial features in face_rec repository
+    print ('starting isForward')
+    face_landmarks_list = face_recognition.face_landmarks(image)
+
+    pil_image = Image.fromarray(image)
+    d = ImageDraw.Draw(pil_image)
+
+    for face_landmarks in face_landmarks_list:
+        # Let's trace out each facial feature in the image with a line!
+        
+        for facial_feature in face_landmarks.keys():
+            if facial_feature == "chin" or facial_feature == "nose_tip":
+                d.line(face_landmarks[facial_feature], width=5)
+                print("The {} in this face has the following points: {}".format(facial_feature, face_landmarks[facial_feature]))
+            
+
+    # Show the picture
+    return pil_image
+
+
+def display(image):
+    image.show()
+
+if __name__ == '__main__':
+    main()
+
--- Gaze Detection/real_time.py
+++ Gaze Detection/real_time.py
@@ -1,7 +1,6 @@
+import face_forward
 import face_recognition
 import cv2
-import face_forward
-import makeup
 
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
@@ -11,11 +10,19 @@ known_face_encodings = []
 known_face_names = []
 
 # Initialize some variables
+storage = "caught/"
+Suspects = "Suspects/"
 face_locations = []
 face_encodings = []
 face_names = []
+suspicion_levels = []
 process_this_frame = True
 suspect_num = 0
+priority = "0"
+solid = True
+
+#We will only anylyze every 5 frames facial features
+frame_count = 0
 
 while True:
     # Grab a single frame of video
@@ -23,6 +30,7 @@ while True:
 
     # Resize frame of video to 1/4 size for faster face recognition processing
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    frame = cv2.resize(frame, (0, 0), fx=2, fy=2)
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_small_frame = small_frame[:, :, ::-1]
@@ -38,46 +46,94 @@ while True:
             # See if the face is a match for the known face(s)
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
             name = "Unknown"
+            
 
             # If a match was found in known_face_encodings, just use the first one.
             if True in matches:
                 first_match_index = matches.index(True)
                 name = known_face_names[first_match_index]
+                # print(str(suspicion_levels[first_match_index]))
+                #only inciment priority once every 10 frames
+                suspicion_levels[first_match_index] += 1
+                if suspicion_levels[first_match_index]%2 == 0:
+                    priority = int(suspicion_levels[first_match_index]/10)
+                    if solid:
+                        solid = False
+                    else:
+                        solid = True
+                    # print(str(priority))
+
+
             #If it wasn't add person jpg
             else:
                 suspect_num += 1
-                cv2.imwrite("Suspects/" + "suspect_" + str(suspect_num) + ".jpg", small_frame)
+                cv2.imwrite(storage + "suspect_" + str(suspect_num) + ".jpg", frame)
                 #load captured image
-                suspect_image = face_recognition.load_image_file("Suspects/" +"suspect_" + str(suspect_num) + ".jpg")
+                suspect_image = face_recognition.load_image_file(storage + "suspect_" + str(suspect_num) + ".jpg")
                 suspect_face_encoding = face_recognition.face_encodings(suspect_image)[0]
+                #Initialize suspition level
+                suspicion_levels.append(0)
+                print("Suspect priorities: "+ str(suspicion_levels))
                 #add it to list of known faces
                 known_face_encodings.append(suspect_face_encoding)
-                known_face_names.append("suspect_" + str(suspect_num))
+                known_face_names.append("SUSPECT_" + str(suspect_num))
             face_names.append(name)
 
     process_this_frame = not process_this_frame
 
-    #test makup
-    frame = makeup.draw(frame)
 
     # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+        # Draw lines on facial features
+        # frame = face_forward.isLookingForward(frame)
+        
+        # Blow the box to the size of video feed
+        top *= 8
+        right *= 8
+        bottom *= 8
+        left *= 8
 
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        priority = int(priority)
+        #Set color of bounding box
+        TextColor = (255, 255, 255)
+        if priority < 5:
+        #Green
+            color = (0,255,0)
+            TextColor = (0, 0, 0)
+
+        elif priority < 15:
+            #yellow
+            color = (0,255,255)
+            TextColor = (0, 0, 0)
+        
+        elif priority < 30:
+            #red
+            color = (0, 0, 255)
+        else:
+            #flashing red/yellow
+            if solid:
+                color = (0, 0, 255)
+            else:
+                color = (0,255,255)
+                TextColor = (0, 0, 0)
+            
+
+        # Draw a bounding box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), color, 2)
+        #cv2.circle(frame, left, 3, (0,0,255), thickness=1, lineType=8, shift=0)
 
         # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        cv2.rectangle(frame, (left, bottom - 70), (right, bottom), color, cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        cv2.putText(frame, name, (left + 6, bottom - 38), font, 1.2, TextColor, 1)
+        cv2.putText(frame, "Priority: " +  str(priority), (left + 6, bottom - 6), font, 0.8, TextColor, 1)
 
     # Display the resulting image
-    cv2.imshow('Video', frame)
+    cv2.imshow('Live Feed', frame)
+
+    if priority == 30:
+        #Send image notification
+        cv2.imwrite(Suspects + "suspect_" + str(suspect_num) + ".jpg", frame)
 
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -86,3 +142,6 @@ while True:
 # Release handle to the webcam
 video_capture.release()
 cv2.destroyAllWindows()
+
+def drawCircle(image, center, radius):
+    cv2.circle(image, center, radius, color, thickness=1, lineType=8, shift=0)
