c605f7e899e5180b33d1afbf8e0a912c4fe7ce13
ryank95 <rkim_mail@yahoo.com>
2018-12-06 23:23:39 -0800

    Update clean_up.py
    - Now skips over suspects if they don't exist
    - No longer relies on order of suspect names

--- Gaze Detection/clean_up.py
+++ Gaze Detection/clean_up.py
@@ -13,47 +13,34 @@ def cleanUp():
     suspect1 = face_recognition.load_image_file(dir + "suspect_1.jpg")
     sus1Encoding = face_recognition.face_encodings(suspect1)[0]
     individual_suspects = [sus1Encoding]
-
-    for i in range(2,fileCount+1):
-        image = face_recognition.load_image_file(dir + "suspect_" + str(i) +".jpg")
-        try:
-            imageEncoding = face_recognition.face_encodings(image)[0]
-        except IndexError:
-            print("Unable to locate any faces")
-            continue
+    i=2
+    # Iterates from suspect_2 to the last suspect
+    for suspects in range(2,fileCount+1):
+        image,i = loadNextImage(i)
+        imageEncoding = face_recognition.face_encodings(image)[0]
             
         # results is a boolean array that compares the encoding with each individual suspect
         results = face_recognition.compare_faces(individual_suspects, imageEncoding)
-        print("suspect_" + str(i) + " new suspect? {}".format(not True in results))
         if(not True in results):
             # suspect is a new individual and is added to the array
             individual_suspects.append(imageEncoding)
         else:
             os.remove(dir + "suspect_" + str(i)+ ".jpg")
+            print("suspect_" + str(i) + " was removed")
+        i+=1
     numIndividualSuspects = len(individual_suspects)
     print("num of individual suspects: " + str(numIndividualSuspects))
-    order()
     
-# UNUSED METHOD
 # Gets the next valid image
 # j is the index of the next valid image
-def loadNextImage(i):
-    j=i
+def loadNextImage(j):
+    i=j
     while os.path.exists(dir):
         try:
-            image = face_recognition.load_image_file(dir + "suspect_" + str(j) +".jpg")
-            return image,j
+            image = face_recognition.load_image_file(dir + "suspect_" + str(i) +".jpg")
+            return image,i
         except FileNotFoundError:
-            j+=1
+            i+=1
             continue
-
-# renames the files so that suspects are numbered incrementally
-def order():
-    i=1
-    for filename in os.listdir(dir):
-        filename = dir + filename
-        name = dir + "suspect_" + str(i) + ".jpg"
-        os.rename(filename, name)
-        i+=1
 
 cleanUp()
